8da8f3fe886e0e9bb2cc771b1b94e11de12ee6d6
William Sutton <tsutton908@gmail.com>
2021-02-13 20:41:51 -0500

    initial styling complete

---
+++ src/Chat.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import ChatHeader from './ChatHeader';
+import Message from './Message';
+
+import './Chat.css';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <ChatHeader />
+
+            <div className="chat__messages">
+                <Message />
+                <Message />
+                <Message />
+            </div>
+
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large" />
+                <form>
+                    <input placeholder={`Message #TESTCHANNEL`} />
+                    <button type="submit" className="chat__inputButton">Send Message</button>
+                </form>
+
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large" />
+                    <GifIcon fontSize="large" />
+                    <EmojiEmotionsIcon fontSize="large" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/Message.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Message.css';
+
+import { Avatar } from '@material-ui/core';
+
+function Message() {
+    return (
+        <div className="message">
+            <Avatar />
+            <div className="message__info">
+                <h4>Suttoast
+                    <span className="message__timestamp">timestamp</span>
+                </h4>
+
+                <p>This is a message</p>
+            </div>
+        </div>
+    )
+}
+
+export default Message
